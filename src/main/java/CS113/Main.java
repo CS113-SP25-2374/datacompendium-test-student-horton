668fba23e628b4ea6172190a150d32b66ebbc646
khorton <test-student@krhorton.com>
2025-02-18 20:18:40 -0800

    Added an ArrayList custom method.

--- src/main/java/CS113/Main.java
+++ src/main/java/CS113/Main.java
@@ -1,7 +1,30 @@
 package CS113;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Integer> goArray = new ArrayList<>();
+        ArrayListPH<Integer> testArray = new ArrayListPH<>();
+
+        for (int i = 0; i < 10; i++) {
+            testArray.add(i);
+            goArray.add(i);
+        }
+
+        goArray.add(4, null);
+        testArray.add(4, null);
+
+        System.out.println(goArray);
+        System.out.println(goArray.size());
+
+        System.out.println(testArray);
+        System.out.println(testArray.size());
+
+        testArray.clear();
+
+
         System.out.println("Hello world!");
     }
 }
